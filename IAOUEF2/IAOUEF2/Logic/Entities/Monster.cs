665bc67f4acf6b6e87be9972642054c46886190d
MaximeCharlet <133816088+CharletMaxime@users.noreply.github.com>
2025-05-17 15:40:24 +0200

    feat: represents entities from the game

---
+++ IAOUEF2/IAOUEF2/Logic/Entities/Monster.cs
@@ -0,0 +1,19 @@
+﻿namespace IAOUEF2.Logic.Entities;
+
+public class Monster
+{
+    /// <summary>
+    /// Nom du monstre
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// Nombre de points de vie
+    /// </summary>
+    public int Hp { get; set; }
+    
+    /// <summary>
+    /// Retournera le nom de points de savoir lors de sa mort
+    /// </summary>
+    public int KnowledgeScore { get; set; }
+}
