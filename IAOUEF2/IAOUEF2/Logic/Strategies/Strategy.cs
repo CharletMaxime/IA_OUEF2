6a8a0e99639467451fe4b9e0fe904cd753d5aacb
Blazyst <159901802+Raoul-schneider@users.noreply.github.com>
2025-05-17 17:20:39 +0200

    feat: ajout utilisation strategie dans program.cs et dossier strategies

---
+++ IAOUEF2/IAOUEF2/Logic/Strategies/Strategy.cs
@@ -0,0 +1,24 @@
+﻿namespace IAOUEF2.Logic.Strategies;
+
+public abstract class Strategy
+{
+    #region Attributes
+        
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Constructor
+
+    public abstract void PlayTurn();
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+
+
+}
---
+++ IAOUEF2/IAOUEF2/Logic/Strategies/StrategySurvival.cs
@@ -0,0 +1,23 @@
+﻿namespace IAOUEF2.Logic.Strategies;
+
+public class StrategySurvival : Strategy
+{
+    #region Attributes
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Constructor
+
+    #endregion
+
+    #region Methods
+    public override void PlayTurn()
+    {
+        
+    }
+    #endregion
+}
